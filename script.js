c22038f93ce1e832959b33ab8169c682b835ed0b
ProficientSammy <121641776+ProficientSammy@users.noreply.github.com>
2023-02-17 18:37:41 -0500

    Complete assignment

---
+++ script.js
@@ -0,0 +1,23 @@
+const Books = [
+{Title:'Amulet' , Author: 'John Davis' , Genre: 'Sci-fi' },
+{Title:'Among The Hidden', Author:'Jax Green', Genre: 'Fiction'}
+]
+
+
+function addBook (Title, Author, Genre) {
+    Books.push({Title, Author, Genre })
+}
+
+function showBooks () {
+    let index = 0
+for (const Book of Books) {
+    console.log (` ${index} .${Book.Title} , ${Book.Title}`)
+    index++
+}
+}
+function showBook (index) {
+    console.log (Books[index])
+    
+
+}
+
